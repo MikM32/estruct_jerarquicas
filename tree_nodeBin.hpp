eccab864eda248c36eb11f833cae4f61a1db6b00
Luis Romero <creytuningct@gmail.com>
2024-06-03 11:59:08 -0400

    serparar nodo hibrido

---
+++ tree_nodeBin.hpp
@@ -0,0 +1,52 @@
+#ifndef NODETREEBIN_HEADER
+#define NODETREEBIN_HEADER
+
+#include "list/list.hpp"
+
+template <typename type>
+class NodeTreeBin{
+private:
+    type value;
+
+    // binary node
+    NodeTreeBin * left = NULL;
+    NodeTreeBin * right = NULL;
+
+public:
+    // constructores y destructor
+    NodeTreeBin() : value(type()), left(NULL), right(NULL) {}
+    NodeTreeBin(type value) : value(value), left(NULL), right(NULL) {}
+    NodeTreeBin(type value, NodeTreeBin * left) : value(value), left(left), right(NULL) {}
+    NodeTreeBin(type value, NodeTreeBin * left, NodeTreeBin * right) : value(value), left(left), right(right) {}
+    ~NodeTreeBin(){};
+
+    // common
+    void setValue(type value) { this->value = value; }
+    type getValue() { return this->value; }
+    type * getPointerToValue() { return &this->value; }
+
+    // binary
+    void setLeft(NodeTreeBin * left) { this->left = left; }
+    void setRight(NodeTreeBin * right) { this->right = right; }
+
+    void setValueToLeft(type value) { this->left->setValue(value); }
+    void setValueToRight(type value) { this->right->setValue(value); }
+
+    NodeTreeBin * getLeft() { return this->left; }
+    NodeTreeBin * getRight() { return this->right; }
+    
+    type getValueOfLeft() { return this->left->getValue(); }
+    type getValueOfRight() { return this->right->getValue(); }
+    
+    int getDegree(){
+        int result = -1;
+
+        if(this->left == NULL && this->right == NULL){result = 0;}
+        else if(this->left == NULL || this->right == NULL){result = 1;}
+        if(this->left != NULL & this->right != NULL){result = 2;}
+
+        return result;
+    }
+};
+
+#endif // NODETREEBIN_HEADER
---
+++ tree_nodeNary.hpp
@@ -0,0 +1,45 @@
+#ifndef NODETREENARY_HEADER
+#define NODETREENARY_HEADER
+
+#include "list/list.hpp"
+
+template <typename type>
+class NodeTreeNary{
+private:
+    type value;
+    
+    // children
+    List<NodeTreeNary*> children = List<NodeTreeNary*>();
+
+public:
+    // constructores y destructor
+    NodeTreeNary() : value(type()){}
+    NodeTreeNary(type value) : value(value){}
+    ~NodeTreeNary(){};
+
+    // common
+    void setValue(type value) { this->value = value; }
+    type getValue() { return this->value; }
+    type * getPointerToValue() { return &this->value; }
+
+    // n-ary
+    void insertAtIndex(int index, NodeTreeNary * child) { this->children.insertAtIndex(index, child); }
+    void insertAtFirst(NodeTreeNary * child) { this->children.insertAtFirst(child); }
+    void insertAtLast(NodeTreeNary * child) { this->children.insertAtLast(child); }
+
+    void removeAtIndex(int index) { this->children.removeAtIndex(index); }
+    void removeAtFirst() { this->children.removeAtFirst(); }
+    void removeAtLast() { this->children.removeAtLast(); }
+    
+    NodeTreeNary * getAtIndex(int index) { return this->children.getValueAtIndex(index); }
+    NodeTreeNary * getFirst() { return this->children.getValueAtFirst(); }
+    NodeTreeNary * getLast() { return this->children.getValueAtLast(); }
+
+    int getDegree() { return this->children.getSize(); }
+
+    void modifyValueOfAtIndex(int index, type value) { this->children.getValueAtIndex(index)->setValue(value); }
+    void modifyValueAtFirst(type value) { this->children.getValueAtFirst()->setValue(value); }
+    void modifyValueAtLast(type value) { this->children.getValueAtLast()->setValue(value); }
+};
+
+#endif // NODETREENARY_HEADER
